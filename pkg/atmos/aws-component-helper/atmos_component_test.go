80b914062e67adf2d69811daf4fb92b3df40ca3c
Igor Rodionov <goruha@gmail.com>
2025-01-06 21:49:46 +0100

    Added comments

--- pkg/atmos/aws-component-helper/atmos_component_test.go
+++ pkg/atmos/aws-component-helper/atmos_component_test.go
@@ -6,10 +6,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// TestAtmosComponent tests the creation and properties of an AtmosComponent
 func TestAtmosComponent(t *testing.T) {
-	// Create a temporary atmos component
+	// Create a temporary AtmosComponent with name "vpc" and stack "default-test"
+	// Passing 'nil' for vars as no specific variables are required for this test
 	component := NewAtmosComponent("vpc", "default-test", nil)
 
+	// Assert that the ComponentName is correctly set to "vpc"
 	assert.Equal(t, component.ComponentName, "vpc")
+
+	// Assert that the StackName is correctly set to "default-test"
 	assert.Equal(t, component.StackName, "default-test")
 }
