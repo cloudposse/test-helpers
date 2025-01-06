a135d28ca119324acd2503adb6879a8b16ebf1fa
Igor Rodionov <goruha@gmail.com>
2025-01-06 21:49:16 +0100

    Added comments

--- pkg/atmos/aws-component-helper/atmos_component.go
+++ pkg/atmos/aws-component-helper/atmos_component.go
@@ -6,20 +6,24 @@ import (
 	"github.com/gruntwork-io/terratest/modules/random"
 )
 
+// Output represents the structure of a Terraform output with optional sensitivity
 type Output struct {
-	Sensitive bool        `json:"sensitive"`
-	Value     interface{} `json:"value"`
+	Sensitive bool        `json:"sensitive"` // Indicates if the output is sensitive
+	Value     interface{} `json:"value"`     // The actual value of the output
 }
 
+// AtmosComponent represents a Terraform component managed by Atmos
 type AtmosComponent struct {
-	randomIdentifier string
-	ComponentName    string
-	StackName        string
-	Vars             map[string]interface{}
-	output           map[string]Output
+	randomIdentifier string                 // A unique identifier for the component
+	ComponentName    string                 // Name of the Terraform component
+	StackName        string                 // Name of the stack in which the component resides
+	Vars             map[string]interface{} // Variables specific to the component
+	output           map[string]Output      // Holds Terraform outputs for the component
 }
 
+// Constructor for AtmosComponent
 func NewAtmosComponent(component string, stack string, vars map[string]interface{}) *AtmosComponent {
+	// Generate a unique random identifier for the component
 	randID := random.UniqueId()
 	randomId := strings.ToLower(randID)
 
@@ -31,6 +35,7 @@ func NewAtmosComponent(component string, stack string, vars map[string]interface
 	}
 }
 
+// GetRandomIdentifier retrieves the unique identifier of the component
 func (ts *AtmosComponent) GetRandomIdentifier() string {
 	return ts.randomIdentifier
 }
