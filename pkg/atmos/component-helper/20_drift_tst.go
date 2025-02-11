d61b9f2c3d7333867fb17986db2ebd72b5550f1b
Igor Rodionov <goruha@gmail.com>
2025-02-11 23:27:10 +0100

    Fix test

---
+++ pkg/atmos/component-helper/20_drift_tst.go
@@ -0,0 +1,28 @@
+package component_helper
+
+import (
+	"strings"
+	"dario.cat/mergo"
+	"github.com/cloudposse/test-helpers/pkg/atmos"
+	"github.com/stretchr/testify/require"
+)
+
+func (s *TestSuite) DriftTest(componentName, stackName string, additionalVars *map[string]interface{}) {
+
+	mergedVars := map[string]interface{}{
+		"attributes": []string{s.Config.RandomIdentifier},
+		"enabled":      false,
+	}
+
+	// Merge in any additional vars passed in
+	if additionalVars != nil {
+		err := mergo.Merge(&mergedVars, additionalVars)
+		require.NoError(s.T(), err)
+	}
+
+	atmosOptions := getAtmosOptions(s.T(), s.Config, componentName, stackName, &mergedVars)
+
+	code, err := atmos.PlanExitCodeE(s.T(), atmosOptions)
+	require.NoError(s.T(), err)
+	require.Equal(s.T(), 0, code)
+}
