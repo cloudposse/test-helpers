eb37de6612b557bb2155afc464966e60db9d222d
Igor Rodionov <goruha@gmail.com>
2025-02-12 23:14:20 +0300

    Update ec2.go

--- pkg/aws/ec2.go
+++ pkg/aws/ec2.go
@@ -10,14 +10,14 @@ import (
 	"github.com/gruntwork-io/terratest/modules/aws"	
 )
 
-func GetNatsByVpcIdE(t *testing.T, vpcId string, awsRegion string) ([]types.NatGateway, error) {
+func GetNatGatewaysByVpcIdE(t *testing.T, ctx context.Context, vpcId string, awsRegion string) ([]types.NatGateway, error) {
 	client, err := aws.NewEc2ClientE(t, awsRegion)
 	if err != nil {
 		return nil, err
 	}
 
 	filter := &types.Filter{Name: awstypes.String("vpc-id"), Values: []string{vpcId}}
-	response, err := client.DescribeNatGateways(context.Background(), &ec2.DescribeNatGatewaysInput{
+	response, err := client.DescribeNatGateways(ctx, &ec2.DescribeNatGatewaysInput{
 		Filter: []types.Filter{*filter},
 	})
 	if err != nil {
