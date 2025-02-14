6d1c014ee9aa27975447a1a2b66495c8c1aab1eb
Igor Rodionov <goruha@gmail.com>
2025-02-15 00:46:03 +0100

    Added ec2 get instances method

--- pkg/aws/ec2.go
+++ pkg/aws/ec2.go
@@ -8,6 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/gruntwork-io/terratest/modules/aws"	
+	"github.com/stretchr/testify/assert"
 )
 
 func GetNatGatewaysByVpcIdE(t *testing.T, ctx context.Context, vpcId string, awsRegion string) ([]types.NatGateway, error) {
@@ -24,4 +25,15 @@ func GetNatGatewaysByVpcIdE(t *testing.T, ctx context.Context, vpcId string, aws
 		return nil, err
 	}
 	return response.NatGateways, nil
+}
+
+// GetPrivateIpsOfEc2InstancesE gets the private IP address of the given EC2 Instance in the given region. Returns a map of instance ID to IP address.
+func GetEc2Instances(t *testing.T, ctx context.Context, instanceID string, awsRegion string) types.Instance {
+	ec2Client := aws.NewEc2Client(t, awsRegion)
+	// TODO: implement pagination for cases that extend beyond limit (1000 instances)
+	input := ec2.DescribeInstancesInput{InstanceIds: []string{instanceID}}
+	output, err := ec2Client.DescribeInstances(ctx, &input)
+	assert.NoError(t, err)
+
+	return output.Reservations[0].Instances[0]
 }
